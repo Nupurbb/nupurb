2ae034879dd570a1fc6d6171e9cab3e79eeba265
EshikaP1 <fairspringgal@gmail.com>
2024-01-29 13:11:24 -0800

    commit-backend-user code-final

--- login.py
+++ login.py
@@ -1,149 +1,66 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-import json
 
-# Setup Flask and SQLAlchemy
 app = Flask(__name__)
-database = 'sqlite:///sqlite.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = database
 app.config['SECRET_KEY'] = 'SECRET_KEY'
-db = SQLAlchemy()
-db.init_app(app)
+db = SQLAlchemy(app)
 
-# Define the User class
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    _name = db.Column(db.String(255), unique=False, nullable=False)
-    _uid = db.Column(db.String(255), unique=True, nullable=False)
-    _password = db.Column(db.String(255), unique=False, nullable=False)
-    _dob = db.Column(db.Date)
+    name = db.Column(db.String(255), nullable=False)
+    uid = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    dob = db.Column(db.Date)
 
-    # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, password="123qwerty", dob=datetime.today()):
-        self._name = name    # variables with self prefix become part of the object, 
-        self._uid = uid
-        self.set_password(password)
-        if isinstance(dob, str):  # not a date type     
-            dob = date=datetime.today()
-        self._dob = dob
+    def __init__(self, name, uid, password, dob=None):
+        self.name = name
+        self.uid = uid
+        self.password = generate_password_hash(password)
+        self.dob = dob
 
-    # a name getter method, extracts name from object
-    @property
-    def name(self):
-        return self._name
-    
-    # a setter function, allows name to be updated after initial object creation
-    @name.setter
-    def name(self, name):
-        self._name = name
-    
-    # a getter method, extracts uid from object
-    @property
-    def uid(self):
-        return self._uid
-    
-    # a setter function, allows uid to be updated after initial object creation
-    @uid.setter
-    def uid(self, uid):
-        self._uid = uid
-        
-    # check if uid parameter matches user id in object, return boolean
-    def is_uid(self, uid):
-        return self._uid == uid
-    
-    @property
-    def password(self):
-        return self._password[0:10] + "..." # because of security only show 1st characters
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
-    # update password, this is conventional method used for setter
-    def set_password(self, password):
-        """Create a hashed password."""
-        self._password = generate_password_hash(password, method='sha256')
-
-    # check password parameter against stored/encrypted password
-    def is_password(self, password):
-        """Check against hashed password."""
-        result = check_password_hash(self._password, password)
-        return result
-    
-    # dob property is returned as string, a string represents date outside object
-    @property
-    def dob(self):
-        dob_string = self._dob.strftime('%m-%d-%Y')
-        return dob_string
-    
-    # dob setter, verifies date type before it is set or default to today
-    @dob.setter
-    def dob(self, dob):
-        if isinstance(dob, str):  # not a date type     
-            dob = date=datetime.today()
-        self._dob = dob
-    
-    # age is calculated field, age is returned according to date of birth
-    @property
-    def age(self):
-        today = datetime.today()
-        return today.year - self._dob.year - ((today.month, today.day) < (self._dob.month, self._dob.day))
-    
-    # output content using str(object) is in human readable form
-    # output content using json dumps, this is ready for API response
-    def __str__(self):
-        return json.dumps(self.read())
-
-    # CRUD create/add a new record to the table
-    # returns self or None on error
-    def create(self):
-        try:
-            # creates a person object from User(db.Model) class, passes initializers
-            db.session.add(self)  # add prepares to persist person object to Users table
-            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
-            return self
-        except IntegrityError:
-            db.session.remove()
-            return None
-
-    # CRUD read converts self to dictionary
-    # returns dictionary
-    def read(self):
+    def serialize(self):
         return {
-            "id": self.id,
-            "name": self.name,
-            "uid": self.uid,
-            "dob": self.dob,
-            "age": self.age,
+            'id': self.id,
+            'name': self.name,
+            'uid': self.uid,
+            'dob': self.dob.strftime('%Y-%m-%d') if self.dob else None
         }
 
-    # CRUD update: updates user name, password, phone
-    # returns self
-    def update(self, name="", uid="", password=""):
-        """only updates values with length"""
-        if len(name) > 0:
-            self.name = name
-        if len(uid) > 0:
-            self.uid = uid
-        if len(password) > 0:
-            self.set_password(password)
-        db.session.add(self) # performs update when id exists
-        db.session.commit()
-        return self
+# Add a route for a simple welcome page
+@app.route('/')
+def welcome():
+    return 'Welcome to the Flask App!'
 
-    # CRUD delete: remove self
-    # None
-    def delete(self):
-        db.session.delete(self)
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    name = data.get('name')
+    uid = data.get('uid')
+    password = data.get('password')
+    dob = datetime.strptime(data.get('dob'), '%Y-%m-%d') if data.get('dob') else None
+
+    user = User(name=name, uid=uid, password=password, dob=dob)
+    try:
+        db.session.add(user)
         db.session.commit()
-        return None
-# Builds working data for testing
-def initUsers():
+        return jsonify(user.serialize()), 201
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({'message': 'User with this UID already exists'}), 400
+
+# Function to initialize users
+def init_users():
     with app.app_context():
-        """Create database and tables"""
         db.create_all()
-        """Tester data for table"""
         u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=datetime(1847, 2, 11))
         u2 = User(name='Nikola Tesla', uid='niko', password='123niko')
         u3 = User(name='Alexander Graham Bell', uid='lex', password='123lex')
@@ -151,70 +68,26 @@ def initUsers():
         u5 = User(name='Indiana Jones', uid='indi', dob=datetime(1920, 10, 21))
         u6 = User(name='Marion Ravenwood', uid='raven', dob=datetime(1921, 10, 21))
 
-
         users = [u1, u2, u3, u4, u5, u6]
 
-        """Builds sample user/note(s) data"""
         for user in users:
             try:
-                '''add user to table'''
-                object = user.create()
-                print(f"Created new uid {object.uid}")
-            except:  # error raised if object nit created
-                '''fails with bad or duplicate data'''
-                print(f"Records exist uid {user.uid}, or error.")
-                
-initUsers()
+                db.session.add(user)
+                db.session.commit()
+                print(f"Created new UID: {user.uid}")
+            except IntegrityError:
+                db.session.rollback()
+                print(f"Record exists for UID: {user.uid} or error.")
 
-def find_by_uid(uid):
-    with app.app_context():
-        user = User.query.filter_by(_uid=uid).first()
-    return user # returns user object
-
-# Check credentials by finding user and verify password
+# Function to check credentials
 def check_credentials(uid, password):
-    # query email and return user record
-    user = find_by_uid(uid)
-    if user == None:
-        return False
-    if (user.is_password(password)):
-        return True
-    return False
-
-def create():
-    # optimize user time to see if uid exists
-    uid = input("Enter your user id:")
-    user = find_by_uid(uid)
-    try:
-        print("Found\n", user.read())
-        return
-    except:
-        pass # keep going
-    
-    # request value that ensure creating valid object
-    name = input("Enter your name:")
-    password = input("Enter your password")
-    
-    # Initialize User object before date
-    user = User(name=name, 
-                uid=uid, 
-                password=password
-                )
-    
-    # create user.dob, fail with today as dob
-    dob = input("Enter your date of birth 'YYYY-MM-DD'")
-    try:
-        user.dob = datetime.strptime(dob, '%Y-%m-%d').date()
-    except ValueError:
-        user.dob = datetime.today()
-        print(f"Invalid date {dob} require YYYY-mm-dd, date defaulted to {user.dob}")
-           
-    # write object to database
     with app.app_context():
-        try:
-            object = user.create()
-            print("Created\n", object.read())
-        except:  # error raised if object not created
-            print("Unknown error uid {uid}")
-        
-create()
+        user = User.query.filter_by(uid=uid).first()
+        if user is None:
+            return False
+        return user.check_password(password)
+
+if __name__ == '__main__':
+    # Uncomment the line below for the first run to initialize users
+    # init_users()
+    app.run(debug=True)
