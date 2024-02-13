a113b7936632ea0dd74912ed299615f1ec81764c
Nupurbb <nupurbhalla08@gmail.com>
2024-02-12 20:57:20 -0800

    fitnessy.py

--- api/fitness.py
+++ api/fitness.py
@@ -1,55 +1,124 @@
-from flask import Flask, render_template
-import markdown2
+import json, jwt
+from flask import Blueprint, request, jsonify, current_app, Response
+from flask_restful import Api, Resource # used for REST API building
+from datetime import datetime
+from auth_middleware import token_required
 
-app = Flask(__name__)
+from model.fitnessy import Fitness
 
-@app.route('/')
-def index():
-    # Read the Markdown content from the file
-    with open('fitness.md', 'r') as markdown_file:
-        markdown_content = markdown_file.read()
+fitness_api = Blueprint('fitness_api', __name__,
+                   url_prefix='/api/fitnessy')
 
-    # Render the Markdown content to HTML
-    html_content = markdown2.markdown(markdown_content)
+# API docs https://flask-restful.readthedocs.io/en/latest/api.html
+api = Api(fitness_api)
 
-    # Pass the HTML content to the template
-    return render_template('index.html', html_content=html_content)
+class FitnessAPI:        
+    class _CRUD(Resource):  # User API operation for Create, Read.  THe Update, Delete methods need to be implemeented
+        @token_required
+        def post(self, current_user): # Create method
+            ''' Read data for json body '''
+            body = request.get_json()
+            
+            ''' Avoid garbage in, error Schecking '''
+            # validate name
+            name = body.get('name')
+            if name is None or len(name) < 2:
+                return {'message': f'Name is missing, or is less than 2 characters'}, 400
+            # validate uid
+            uid = body.get('uid')
+            if uid is None or len(uid) < 2:
+                return {'message': f'User ID is missing, or is less than 2 characters'}, 400
+            # look for password and dob
+            password = body.get('password')
+            dob = body.get('dob')
 
-@app.route('/recommendation', methods=['POST'])
-def get_recommendation():
-    try:
-        # Retrieve user inputs from the form
-        age = int(request.form['age'])
-        weight = float(request.form['weight'])
-        height = float(request.form['height'])
-        gender = request.form['gender']
-        activity_level = request.form['activity_level']
+            ''' #1: Key code block, setup USER OBJECT '''
+            uo = User(name=name, 
+                      uid=uid)
+            
+            ''' Additional garbage error checking '''
+            # set password if provided
+            if password is not None:
+                uo.set_password(password)
+            # convert to date type
+            if dob is not None:
+                try:
+                    uo.dob = datetime.strptime(dob, '%Y-%m-%d').date()
+                except:
+                    return {'message': f'Date of birth format error {dob}, must be mm-dd-yyyy'}, 400
+            
+            ''' #2: Key Code block to add user to database '''
+            # create user in database
+            user = uo.create()
+            # success returns json of user
+            if user:
+                return jsonify(user.read())
+            # failure returns error
+            return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 400
 
-        # Calculate BMI
-        bmi = calculate_bmi(weight, height)
+        @token_required
+        def get(self, current_user): # Read Method
+            users = User.query.all()    # read/extract all users from database
+            json_ready = [user.read() for user in users]  # prepare output in json
+            return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
+    
+    class _Security(Resource):
+        def post(self):
+            try:
+                body = request.get_json()
+                if not body:
+                    return {
+                        "message": "Please provide user details",
+                        "data": None,
+                        "error": "Bad request"
+                    }, 400
+                ''' Get Data '''
+                uid = body.get('uid')
+                if uid is None:
+                    return {'message': f'User ID is missing'}, 400
+                password = body.get('password')
+                
+                ''' Find user '''
+                user = User.query.filter_by(_uid=uid).first()
+                if user is None or not user.is_password(password):
+                    return {'message': f"Invalid user id or password"}, 400
+                if user:
+                    try:
+                        token = jwt.encode(
+                            {"_uid": user._uid},
+                            current_app.config["SECRET_KEY"],
+                            algorithm="HS256"
+                        )
+                        resp = Response("Authentication for %s successful" % (user._uid))
+                        resp.set_cookie("jwt", token,
+                                max_age=3600,
+                                secure=True,
+                                httponly=True,
+                                path='/',
+                                samesite='None'  # This is the key part for cross-site requests
 
-        # Determine fitness recommendation based on BMI and activity level
-        recommendation = generate_recommendation(bmi, activity_level)
+                                # domain="frontend.com"
+                                )
+                        return resp
+                    except Exception as e:
+                        return {
+                            "error": "Something went wrong",
+                            "message": str(e)
+                        }, 500
+                return {
+                    "message": "Error fetching auth token!",
+                    "data": None,
+                    "error": "Unauthorized"
+                }, 404
+            except Exception as e:
+                return {
+                        "message": "Something went wrong!",
+                        "error": str(e),
+                        "data": None
+                }, 500
 
-        return render_template('recommendation.html', recommendation=recommendation)
-
-    except ValueError:
-        error_message = "Invalid input. Please enter valid numerical values."
-        return render_template('error.html', error_message=error_message)
-
-def calculate_bmi(weight, height):
-    return weight / ((height / 100) ** 2)
-
-def generate_recommendation(bmi, activity_level):
-    if bmi < 18.5:
-        return "You are underweight. Consider increasing your calorie intake and doing strength training exercises."
-    elif bmi < 25:
-        if activity_level == 'sedentary':
-            return "Your BMI is within the healthy range. Try to incorporate more physical activity into your daily routine."
-        else:
-            return "Your BMI is within the healthy range. Keep up the good work with your current activity level."
-    else:
-        return "You are overweight. Focus on a balanced diet and increase your physical activity level."
-
-if __name__ == '__main__':
-    app.run(debug=True)
+            
+    # building RESTapi endpoint
+    api.add_resource(_CRUD, '/')
+    api.add_resource(_Security, '/authenticate')
+    
