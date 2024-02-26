9c10ca20d31058f602505d856235855dbc2e37ef
Nupurbb <nupurbhalla08@gmail.com>
2024-02-26 11:21:13 -0800

    model change

--- api/fitness.py
+++ api/fitness.py
@@ -1,124 +1,66 @@
-import json, jwt
+import json
 from flask import Blueprint, request, jsonify, current_app, Response
-from flask_restful import Api, Resource # used for REST API building
+from flask_restful import Api, Resource
 from datetime import datetime
 from auth_middleware import token_required
 
 from model.fitnessy import Fitness
 
-fitness_api = Blueprint('fitness_api', __name__,
-                   url_prefix='/api/fitnessy')
+fitness_api = Blueprint('fitness_api', __name__, url_prefix='/api/fitness')
 
-# API docs https://flask-restful.readthedocs.io/en/latest/api.html
 api = Api(fitness_api)
 
-class FitnessAPI:        
-    class _CRUD(Resource):  # User API operation for Create, Read.  THe Update, Delete methods need to be implemeented
+class FitnessAPI:
+    class _CRUD(Resource):
         @token_required
-        def post(self, current_user): # Create method
-            ''' Read data for json body '''
+        def post(self, current_user):
             body = request.get_json()
-            
-            ''' Avoid garbage in, error Schecking '''
-            # validate name
             name = body.get('name')
             if name is None or len(name) < 2:
                 return {'message': f'Name is missing, or is less than 2 characters'}, 400
-            # validate uid
-            uid = body.get('uid')
-            if uid is None or len(uid) < 2:
-                return {'message': f'User ID is missing, or is less than 2 characters'}, 400
-            # look for password and dob
-            password = body.get('password')
-            dob = body.get('dob')
-
-            ''' #1: Key code block, setup USER OBJECT '''
-            uo = User(name=name, 
-                      uid=uid)
             
-            ''' Additional garbage error checking '''
-            # set password if provided
-            if password is not None:
-                uo.set_password(password)
-            # convert to date type
-            if dob is not None:
-                try:
-                    uo.dob = datetime.strptime(dob, '%Y-%m-%d').date()
-                except:
-                    return {'message': f'Date of birth format error {dob}, must be mm-dd-yyyy'}, 400
+            # Adjustments for fitness data
+            age = body.get('age')
+            weight = body.get('weight')
+            height = body.get('height')
+            gender = body.get('gender')
+            activity_level = body.get('activity_level')
+
+            # Example validation
+            if age is None or age <= 0:
+                return {'message': 'Invalid age'}, 400
+            if weight is None or weight <= 0:
+                return {'message': 'Invalid weight'}, 400
+            if height is None or height <= 0:
+                return {'message': 'Invalid height'}, 400
+            if gender not in ['male', 'female']:
+                return {'message': 'Invalid gender'}, 400
+            if activity_level not in ['sedentary', 'lightly_active', 'moderately_active', 'very_active', 'extra_active']:
+                return {'message': 'Invalid activity level'}, 400
             
-            ''' #2: Key Code block to add user to database '''
-            # create user in database
-            user = uo.create()
-            # success returns json of user
-            if user:
-                return jsonify(user.read())
-            # failure returns error
-            return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 400
+            # Create Fitness object
+            fitness_data = {
+                'name': name,
+                'age': age,
+                'weight': weight,
+                'height': height,
+                'gender': gender,
+                'activity_level': activity_level
+            }
+            new_fitness_record = Fitness(**fitness_data)
 
-        @token_required
-        def get(self, current_user): # Read Method
-            users = User.query.all()    # read/extract all users from database
-            json_ready = [user.read() for user in users]  # prepare output in json
-            return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
-    
-    class _Security(Resource):
-        def post(self):
-            try:
-                body = request.get_json()
-                if not body:
-                    return {
-                        "message": "Please provide user details",
-                        "data": None,
-                        "error": "Bad request"
-                    }, 400
-                ''' Get Data '''
-                uid = body.get('uid')
-                if uid is None:
-                    return {'message': f'User ID is missing'}, 400
-                password = body.get('password')
-                
-                ''' Find user '''
-                user = User.query.filter_by(_uid=uid).first()
-                if user is None or not user.is_password(password):
-                    return {'message': f"Invalid user id or password"}, 400
-                if user:
-                    try:
-                        token = jwt.encode(
-                            {"_uid": user._uid},
-                            current_app.config["SECRET_KEY"],
-                            algorithm="HS256"
-                        )
-                        resp = Response("Authentication for %s successful" % (user._uid))
-                        resp.set_cookie("jwt", token,
-                                max_age=3600,
-                                secure=True,
-                                httponly=True,
-                                path='/',
-                                samesite='None'  # This is the key part for cross-site requests
+            # Example: saving to database
+            new_fitness_record.save()
 
-                                # domain="frontend.com"
-                                )
-                        return resp
-                    except Exception as e:
-                        return {
-                            "error": "Something went wrong",
-                            "message": str(e)
-                        }, 500
-                return {
-                    "message": "Error fetching auth token!",
-                    "data": None,
-                    "error": "Unauthorized"
-                }, 404
-            except Exception as e:
-                return {
-                        "message": "Something went wrong!",
-                        "error": str(e),
-                        "data": None
-                }, 500
+            return jsonify({'message': 'Fitness data saved successfully'}), 201
 
-            
-    # building RESTapi endpoint
+        def get(self, current_user):
+            # Example: Retrieving fitness data
+            fitness_records = Fitness.query.all()
+            json_ready = [record.serialize() for record in fitness_records]
+            return jsonify(json_ready)
+
     api.add_resource(_CRUD, '/')
-    api.add_resource(_Security, '/authenticate')
-    
+
+
+
--- model/fitnessy.py
+++ model/fitnessy.py
@@ -8,121 +8,71 @@ from __init__ import app, db
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
-''' Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along '''
-
-# Define the drinks class to manage actions in the 'drinks' table
-# -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
-# -- a.) db.Model is like an inner layer of the onion in ORM
-# -- b.) Drink represents data we want to store, something that is built on db.Model
-# -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
 class Fitness(db.Model):
-    __tablename__ = 'fitnessy'  # table name is plural, class name is singular
+    __tablename__ = 'fitness'
 
-    # Define the Drink schema with "vars" from object
-    _fitnessName = db.Column(db.String(255), primary_key=True)
-    _age = db.Column(db.Integer, unique=False, nullable=False)
+    _id = db.Column(db.Integer, primary_key=True)
+    _name = db.Column(db.String(255), nullable=False)
+    _age = db.Column(db.Integer, nullable=False)
+    _weight = db.Column(db.Float, nullable=False)
+    _height = db.Column(db.Float, nullable=False)
+    _gender = db.Column(db.String(10), nullable=False)
+    _activity_level = db.Column(db.String(20), nullable=False)
 
-    # constructor of a Drink object, initializes the instance variables within object (self)
-    def __init__(self, fitnessName, age):
-        self._fitnessName = fitnessName    # variables with self prefix become part of the object, 
+    def __init__(self, name, age, weight, height, gender, activity_level):
+        self._name = name
         self._age = age
+        self._weight = weight
+        self._height = height
+        self._gender = gender
+        self._activity_level = activity_level
 
-
-    # a drinkName getter method, extracts drinkName from object
     @property
-    def fitnessName(self):
-        return self._fitnessName
-    
-    # a setter function, allows drinkName to be updated after initial object creation
-    @fitnessName.setter
-    def fitnessName(self, fitnessName):
-        self._fitnessName = fitnessName
-    
-    # a getter method, extracts calories from object
+    def name(self):
+        return self._name
+
     @property
     def age(self):
         return self._age
-    
-    # a setter function, allows calories to be updated after initial object creation
-    @age.setter
-    def age(self, age):
-        self._age = age
-          
-    # output content using str(object) in human readable form, uses getter
-    # output content using json dumps, this is ready for API response
-    def __str__(self):
-        return json.dumps(self.read())
 
-    # CRUD create/add a new record to the table
-    # returns self or None on error
-    def create(self):
-        try:
-            # creates a drink object from Drink(db.Model) class, passes initializers
-            db.session.add(self)  # add prepares to persist drink object to drinks table
-            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
-            return self
-        except IntegrityError:
-            db.session.remove()
-            return None
+    @property
+    def weight(self):
+        return self._weight
 
-    # CRUD read converts self to dictionary
-    # returns dictionary
-    def read(self):
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def gender(self):
+        return self._gender
+
+    @property
+    def activity_level(self):
+        return self._activity_level
+
+    def serialize(self):
         return {
-            "fitnessName": self.fitnessName,
-            "age": self.age
+            "name": self._name,
+            "age": self._age,
+            "weight": self._weight,
+            "height": self._height,
+            "gender": self._gender,
+            "activity_level": self._activity_level
         }
 
-    # CRUD update: updates user name, password, phone
-    # returns self
-    def update(self, fitnessName="", age=0):
-        """only updates values with length"""
-        if len(fitnessName) > 0:
-            self.fitnessName = fitnessName
-        if age >= 0:
-            self.age = age
-        db.session.commit()
-        return self
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return True
+        except IntegrityError:
+            db.session.rollback()
+            return False
 
-    # CRUD delete: remove self
-    # None
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-        return None
-
-
-"""Database Creation and Testing """
-
-
-# Builds working data for testing
 def initFitnessy():
     with app.app_context():
-        """Create database and tables"""
         db.create_all()
-        """Tester data for table"""
-        #f1 = Fitnessy(fitnessName='run', age=10)
-        #f2 = Fitnessy(drinkName='Pilates', age=20)
+        # Add initialization data if needed
 
-        fitnessytoadd = []
-        try:
-            with open(r'fitness.json','r') as json_file:
-                data = json.load(json_file)
-        except Exception as error:
-            print("failed")
 
-        for item in data:
-            print(item)
-            f_toadd = Fitness(FitnessName=item['fitnessName'], age=item['age'])
-            fitnessytoadd.append(f_toadd)
-
-        """Builds sample user/note(s) data"""
-        for f in fitnessytoadd:
-            try:
-                f.create()
-            except IntegrityError:
-                '''fails with bad or duplicate data'''
-                db.session.remove()
-                print(f"Records exist, duplicate email, or error: {f.fitnessytoadd}")
-            
